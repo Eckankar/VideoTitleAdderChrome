424b4d8126b285c9c12c7651421404a535d986cc
Sebastian Paaske Tørholm <Eckankar+github@gmail.com>
2012-02-19 20:21:51 +0100

    Complete rewrite for v1.2.0
    Rewrite to use jQuery.
    Added tooltips.

--- script.js
+++ script.js
@@ -1,63 +1,150 @@
-var ytcollapseimg = "data:image/gif,GIF89a%0B%00%0B%00%A2%00%00%00%00%00%FF%FF%FF%5C%AD%E0%B6%E3%FF%FF%A5%A5%CE%CE%CE%FF%FF%FF%00%00%00!%F9%04%01%00%00%06%00%2C%00%00%00%00%0B%00%0B%00%00%03%24h%B0%CC%3A0%8E%10%96%94%15%5Ci%1B%1B%1DA%05%04Pt%9E%B9%08C%E1%BE'%20%B0%DB%3A%D7J%BA%18%09%00%3B";
-var ytexpandimg = "data:image/gif,GIF89a%0B%00%0B%00%A2%00%00%00%00%00%FF%FF%FF%5C%AD%E0%B6%E3%FF%FF%A5%A5%CE%CE%CE%FF%FF%FF%00%00%00!%F9%04%01%00%00%06%00%2C%00%00%00%00%0B%00%0B%00%00%03*h%B0%CC%3A0%8E%10Vd%B0%82K%40%81%16%A3m!A%05%5D%11.%A3%0A%08%60Z%B8%02%CC%7C%C3R%C33%0E(%8D%86!%01%00%3B";
-var ytdisabled = "data:image/gif,GIF89a%0B%00%0B%00%A2%00%00%00%00%00%FF%FF%FF%5C%AD%E0%B6%E3%FF%FF%A5%A5%CE%CE%CE%FF%FF%FF%00%00%00!%F9%04%01%00%00%06%00%2C%00%00%00%00%0B%00%0B%00%00%03-h%B0%CC%3A0%8E%10%16%04%11W%0C%08(%DDg%0D%40U%15%20v%9D%05%A9%96p%BB%08!%E8%A5%02%8D%92%E8%9C%B7%10%99%A2%D10%24%00%00%3B";
+var YTTA = function () {};
 
-// Added as an additional class to links that've already been worked on.
-var MAGIC_CLASSNAME = 'ytta_chrome_beenhere_already';
+YTTA.URLREGEX = /(?:youtube\.com\/watch\?.*v=|youtu\.be\/)([-_A-Za-z0-9]{11})/i;
+YTTA.ATTR_ID = 'data-ytta-id';
+YTTA.ATTR_EMBED_ID = 'data-ytta-embed-id';
+YTTA.ATTR_ORIG_HTML = 'data-ytta-html';
+YTTA.ATTR_VISITED = 'data-ytta-visited';
+YTTA.ATTR_EMBEDCODE = 'data-ytta-embedcode';
 
-var ytlinks, ytimage, ytembedimage, ytreplacename;
+YTTA.CLASS_UPVOTES = 'ytta-vote-up';
+YTTA.CLASS_DOWNVOTES = 'ytta-vote-down';
+YTTA.CLASS_EMBED_ICON = 'ytta-embed-icon';
+YTTA.CLASS_EMBED_DISABLED = 'ytta-embed-disabled';
+YTTA.CLASS_EMBED_ENABLED = 'ytta-embed-enabled';
 
-chrome.extension.sendRequest({name : 'options'}, function (resp) {
-    console.log(resp);
+YTTA.EXTENSION_ID = chrome.i18n.getMessage('@@extension_id');
+YTTA.EMBED_IMG = 'chrome-extension://' + YTTA.EXTENSION_ID + '/icons/embed.ico';
 
-    ytlinks = resp["textlinks"] ? resp["textlinks"]*1 : 1;
-    ytimage = resp["imglinks"] ? resp["imglinks"]*1 : 0;
-    ytembedimage = resp["embed"] ? resp["embed"]*1 : 1;
-    ytreplacename = resp["replacename"] ? resp["replacename"]*1 : 0;
 
-    youtubifyLinks();
+$(document).ready(function () {
+    chrome.extension.sendRequest({name : 'options'}, function (resp) {
+        YTTA.links = resp["textlinks"] ? resp["textlinks"]*1 : 1;
+        YTTA.image = resp["imglinks"] ? resp["imglinks"]*1 : 0;
+        YTTA.embedimage = resp["embed"] ? resp["embed"]*1 : 1;
+        YTTA.replacename = resp["replacename"] ? resp["replacename"]*1 : 0;
+        YTTA.tooltip = resp["tooltip"] ? resp["tooltip"]*1 : 1;
+
+        $('body').on('DOMNodeInserted', function (e) {
+            youtubifyLinks(e.target);
+        });
+
+        youtubifyLinks('body');
+    });
 });
 
-var allLinks, thisLink, thetitle, tubelink;
+function youtubifyLinks(target) {
+    $(target).find('a[href]:not(['+YTTA.ATTR_ID+'])').each( function (i, elm) {
+        var e = $(elm);
+        e.attr(YTTA.ATTR_ID, '-');
 
-{
-    var it = document.evaluate('//a[@href]', document, null, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
-    allLinks = new Array();
-    var link;
-    while (link = it.iterateNext()) {
-        allLinks.push(link);
-    }
-}
+        var match = e.attr('href').match(YTTA.URLREGEX);
+        if (match) {
+            var id = match[1];
+            e.attr(YTTA.ATTR_ID, id);
+            e.attr(YTTA.ATTR_ORIG_HTML, e.html());
 
-function youtubifyLinks() {
-    for (var i = 0; i < allLinks.length; i++) {
-        var link = allLinks[i];
-
-        if (link.href.search('youtube.com/watch') > -1) {
-            var theURL = unescape(link.href);
-            var videoid = theURL.match(/youtube\.com\/watch\?.*v=([-_A-Za-z0-9]{11})/)[1];
-            chrome.extension.sendRequest({name: "fetchPage", videoid: videoid, i: i}, function(resp) {
-                addtitle(resp.i, resp.response, resp.videoid);
-            });
-        } else if (link.href.search('youtu.be/') > -1) {
-            var theURL = unescape(link.href);
-            var videoid = theURL.match(/youtu\.be\/([-_A-Za-z0-9]{11})/)[1];
-            chrome.extension.sendRequest({name: "fetchPage", videoid: videoid, i: i}, function(resp) {
-                addtitle(resp.i, resp.response, resp.videoid);
-            });
-
+            chrome.extension.sendRequest({
+                "name": "fetchPage",
+                "videoid": id,
+            }, addTitle);
         }
+    });
+}
+
+function addTitle(resp) {
+    var gdata = resp.response;
+    var id = resp.videoid;
+
+    console.log(gdata);
+    var title = gdata.entry.title.$t;
+    var thumbnails = getThumbnails(gdata);
+    var upvotepercent = getUpvotePercent(gdata);
+
+    var e = $('a['+YTTA.ATTR_ID+'='+id+']:not(['+YTTA.ATTR_VISITED+'])').first();
+    e.attr(YTTA.ATTR_VISITED, 'true');
+
+    var isImage = e.find('img').length > 0;
+    if (YTTA.links && !isImage || YTTA.image && isImage) {
+        var text = '<b>YT: ' + title + '</b>';
+
+        var orightml = e.attr(YTTA.ATTR_ORIG_HTML);
+        if (!YTTA.replacename || orightml.search('http://') == -1) {
+            text = orightml + " (" + text + ")";
+        }
+
+        text = '<i>'+text+'</i>';
+        e.html(text);
+    }
+
+    if (YTTA.embedimage) {
+        var starttime = extractTime(e.attr('href'));
+        var embed = embedCode(id, starttime);
+
+        e.after('<img src="' + YTTA.EMBED_IMG + '" ' +
+                'class="' + YTTA.CLASS_EMBED_ICON + '" ' +
+                'title="Click to play video inline." />' +
+                '<div class="' + YTTA.CLASS_EMBED_DISABLED + '">' +
+                    embed +
+                '</div>');
+
+
+        var embedimg = e.next();
+        embedimg.on('click', function (e) {
+            var embeddiv = $(e.target).next();
+            if (embeddiv.hasClass(YTTA.CLASS_EMBED_DISABLED)) {
+                embeddiv.removeClass(YTTA.CLASS_EMBED_DISABLED);
+                embeddiv.addClass(YTTA.CLASS_EMBED_ENABLED);
+            } else {
+                embeddiv.removeClass(YTTA.CLASS_EMBED_ENABLED);
+                embeddiv.addClass(YTTA.CLASS_EMBED_DISABLED);
+            }
+        });
+    }
+
+    if (YTTA.tooltip) {
+        var tooltip = "<h1>" + title + "</h1>";
+
+        tooltip += '<div>';
+        for (var i = 0; i < thumbnails.length; i++) {
+            var thumb = thumbnails[i];
+            console.log(thumb);
+            tooltip += '<img src="' + thumb['url'] + '" ' +
+                       'width=' + thumb['width'] + ' ' +
+                       'height=' + thumb['height'] + '/>';
+        }
+        tooltip += '</div>';
+        tooltip += '<div class="'+YTTA.CLASS_DOWNVOTES+'">';
+        tooltip += '<div class="'+YTTA.CLASS_UPVOTES+'" '+
+                   'style="width: '+upvotepercent+'%"></span>';
+        tooltip += '</div>';
+
+        e.simpletip( {
+            baseClass: 'ytta-tooltip',
+            content: tooltip
+        } );
     }
 }
 
-function addtitle(number, response, videoid) {
-    var link = allLinks[number];
+function getThumbnails(gdata) {
+    var rawThumbs = gdata.entry.media$group.media$thumbnail;
+    var thumbs = [];
 
-    var videoname = response.entry.title.$t;
+    for (var i = 1; i < rawThumbs.length; i++) {
+        thumbs.push(rawThumbs[i]);
+    }
+    return thumbs;
+}
 
-    var starttime = 0;
-    if (link.href.search("#t=") > -1) {
-        var tsmatch = link.href.match(/#t=(?:(\d*)h)?(?:(\d*)m)?(\d*)s?/);
+function getUpvotePercent(gdata) {
+    var rating = gdata.entry.gd$rating;
+    return 100*(rating.average - rating.min)/(rating.max - rating.min)
+}
+
+function extractTime(url) {
+    var time = 0;
+    if (url.search("#t=") > -1) {
+        var tsmatch = url.match(/#t=(?:(\d*)h)?(?:(\d*)m)?(\d*)s?/);
         var sec = 0;
         if (tsmatch[1]) {
             sec += tsmatch[1] * 60 * 60;
@@ -69,90 +156,22 @@ function addtitle(number, response, videoid) {
             sec += tsmatch[3] * 1;
         }
 
-        starttime = sec;
+        time = sec;
     }
 
-    var videoembed =
-        '<object width="425" height="355">' +
-        '<param name="movie" ' +
-                'value="http://www.youtube.com/v/' + videoid + '?version=3&autohide=1&showinfo=0&fs=1&start=' + starttime + '"></param>' +
-        '<param name="allowScriptAccess" value="always"></param>' +
-        '<param name="allowFullScreen" value="true"></param>' +
-        '<embed src="http://www.youtube.com/v/' + videoid + '?version=3&autohide=1&showinfo=0&fs=1&start=' + starttime + '" ' +
-                'type="application/x-shockwave-flash" ' +
-                'allowscriptaccess="always" ' +
-                'allowfullscreen="true" ' +
-                'width="425" height="355"></embed>' +
-        '</object>';
-
-    var embeddedtag = document.createElement('div');
-    var btn = document.createElement('a');
-
-    var alink = videoname + number;
-    btn.id = alink;
-    var embedid = alink + "embedded";
-    embeddedtag.id = embedid;
-    embeddedtag.style.display = 'none';
-    embeddedtag.setAttribute("align", "center");
-    embeddedtag.innerHTML = videoembed;
-    btn.innerHTML = '<img src="' + ytexpandimg + '" align="top" border="0" title="Click to Show Video" style="display: inline; padding-left: 3px">';
-    btn.addEventListener('click', function(){toggleembed(this.id,1);}, true);
-
-    var childImgs = link.getElementsByTagName('img');
-    var isImgLink = childImgs.length > 0;
-
-    if ((isImgLink && ytimage) || (!isImgLink && ytlinks)) {
-        if (ytreplacename && link.innerHTML.search('http://') > -1) {
-            link.innerHTML = '<i><b>YT: ' + videoname + '</b></i>';
-        } else {
-            link.innerHTML = '<i>' + link.innerHTML + " (<b>YT: " + videoname + "</b>)</i>";
-        }
-    }
-
-    if (ytembedimage == 1) {
-        link.parentNode.insertBefore(embeddedtag, link.nextSibling);
-        link.parentNode.insertBefore(btn, link.nextSibling);
-    }
-
-    if (isImgLink && !ytimage) {
-        for (var i = 0; i < childImgs.length; i++) {
-            childImgs[i].setAttribute('title', 'YouTube link: Video name = "' + videoname + '"');
-        }
-    }
-
-    link.setAttribute('title', 'YouTube link: Video name = "' + videoname + '"');
+    return time;
 }
 
-function toggleembed(id,embedable)
-{
-	embeddedid = id + "embedded";
-	isembedded = document.getElementById(embeddedid);
-	if (isembedded.style.display != 'none')
-	{
-		if (embedable == 1) document.getElementById(id).innerHTML = '<img src="' + ytexpandimg + '" align="top" border="0" title="Click to Show Video" style="display: inline">';
-		if (embedable == 0) document.getElementById(id).innerHTML = '<img src="' + ytdisabled + '" align="top" border="0" title="Embedding disabled for this video, click to see preview thumb" style="display: inline">';
-		isembedded.style.display = 'none';
-	}
-	else
-	{
-		document.getElementById(id).innerHTML = '<img src="' + ytcollapseimg + '" align="top" border="0" title="Click to Hide Video" style="display: inline">';
-		isembedded.style.display = '';
-	}
-}
-
-function unescapeHTML(s)
-{
-	return s.replace(
-	  /&(amp|[lg]t|quot);/g,
-	  function(m, p1)
-	  {
-		var map = {
-		  amp:  "&",
-		  lt:   "<",
-		  gt:   ">",
-		  quot: '"'
-		};
-
-		return map[p1];
-	  });
+function embedCode(id, time) {
+    return '<object width="425" height="355">' +
+           '<param name="movie" ' +
+                   'value="http://www.youtube.com/v/' + id + '?version=3&autohide=1&showinfo=0&fs=1&start=' + time + '"></param>' +
+           '<param name="allowScriptAccess" value="always"></param>' +
+           '<param name="allowFullScreen" value="true"></param>' +
+           '<embed src="http://www.youtube.com/v/' + id + '?version=3&autohide=1&showinfo=0&fs=1&start=' + time + '" ' +
+                   'type="application/x-shockwave-flash" ' +
+                   'allowscriptaccess="always" ' +
+                   'allowfullscreen="true" ' +
+                   'width="425" height="355"></embed>' +
+           '</object>';
 }
